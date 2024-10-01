bb35b7530dc88d077a8e74233e08c1fae6132597
Julien <julien.etelain@outlook.com>
2024-10-01 22:34:45 +0200

    Setup script

---
+++ cTabIRL/setup/setup.sh
@@ -0,0 +1,57 @@
+#!/bin/sh
+
+# Basic script to install / update a ctab instance on a Linux server
+# Requires dotnet SDK, see https://learn.microsoft.com/en-us/dotnet/core/install/linux, `sudo apt-get install -y dotnet-sdk-8.0` on Ubuntu 24.04 LTS
+
+if [ ! -d ~/build/ctab ]; then
+	mkdir ~/build
+	cd ~/build
+	git clone https://github.com/jetelain/ctab.git ctab
+fi
+
+if [ ! -d /opt/ctab ]; then
+	sudo mkdir /opt/ctab
+	sudo chown $USER:$USER /opt/ctab
+fi
+
+if [ ! -d /var/www/ctab ]; then
+	sudo mkdir /var/www/ctab
+	sudo chown www-data:www-data /var/www/ctab
+fi
+
+if [ ! -d /var/www/aspnet-keys ]; then
+	sudo mkdir /var/www/aspnet-keys
+	sudo chown www-data:www-data /var/www/aspnet-keys
+fi
+
+cd ~/build/ctab
+
+echo "Update git"
+git checkout main
+git pull
+
+echo "Check config"
+if [ ! -f /opt/ctab/appsettings.Production.json ]; then
+	echo " * Create appsettings.Production.json"
+	cp cTabIRL/setup/appsettings.Production.json /opt/ctab/appsettings.Production.json
+	read -p "Type the Steam Api Key obtained from https://steamcommunity.com/dev/apikey, then press [ENTER]:" STEAM_API_KEY
+	sed -i "s/STEAM_API_KEY/$STEAM_API_KEY/g"  /opt/ctab/appsettings.Production.json
+fi
+
+if [ ! -f /etc/systemd/system/kestrel-ctab.service ]; then
+	echo " * Create kestrel-ctab.service"
+	sudo cp cTabIRL/setup/kestrel-ctab.service /etc/systemd/system/kestrel-ctab.service
+fi
+
+echo "Build"
+rm -rf dotnet-webapp
+dotnet publish -c ReleaseCloud -o dotnet-webapp -r linux-x64 --self-contained false cTabIRL/cTabWebApp/cTabWebApp.csproj
+
+echo "Stop Service"
+sudo systemctl stop kestrel-ctab
+
+echo "Copy files"
+cp -ar "dotnet-webapp/." "/opt/ctab"
+
+echo "Start Service"
+sudo systemctl start kestrel-ctab
