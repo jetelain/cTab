bb60f8b4a85cffdb77828584fe65e7a82f79ff68
Julien <julien.etelain@outlook.com>
2025-01-17 22:24:46 +0100

    Restore cache

--- @cTab/addons/messaging/script_component.hpp
+++ @cTab/addons/messaging/script_component.hpp
@@ -1,8 +1,8 @@
 #define COMPONENT messaging
 #include "\z\ctab\addons\main\script_mod.hpp"
 
- #define DEBUG_MODE_FULL
- #define DISABLE_COMPILE_CACHE
+// #define DEBUG_MODE_FULL
+// #define DISABLE_COMPILE_CACHE
 
 #ifdef DEBUG_ENABLED_CORE
     #define DEBUG_MODE_FULL
